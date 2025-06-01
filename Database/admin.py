883e73644bff1372636af02cde637869aea2a6fd
Hero Software <herosoftware@herofashion.com>
2025-06-01 12:24:16 +0530

    Add Demo Class

--- Database/admin.py
+++ Database/admin.py
@@ -1,1 +1,10 @@
 from django.contrib import admin
+from .models import Demo
+
+class DemoAdmin(admin.ModelAdmin):
+    list_display = ('name', 'age')
+    list_filter = ('name', 'age')
+    search_fields = ('name', 'age')
+    ordering = ('name', 'age')
+
+admin.site.register(Demo, DemoAdmin)
--- Database/models.py
+++ Database/models.py
@@ -1,1 +1,8 @@
 from django.db import models
+
+class Demo(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    
+    def __str__(self):
+        return self.name
