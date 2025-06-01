d55a41544fbb1307dfa46558968fb44e45ca36b4
Hero Developers <134581766+Herosoftware01@users.noreply.github.com>
2025-06-01 13:05:57 +0530

    add salary

--- Database/admin.py
+++ Database/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from .models import Demo
 
 class DemoAdmin(admin.ModelAdmin):
-    list_display = ('name', 'age')
-    list_filter = ('name', 'age')
-    search_fields = ('name', 'age')
-    ordering = ('name', 'age')
+    list_display = ('name', 'age','salary')
+    list_filter = ('name', 'age','salary')
+    search_fields = ('name', 'age','salary')
+    ordering = ('name', 'age','salary')
 
 admin.site.register(Demo, DemoAdmin)
--- Database/models.py
+++ Database/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Demo(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
+    salary = models.IntegerField()
     
     def __str__(self):
         return self.name
